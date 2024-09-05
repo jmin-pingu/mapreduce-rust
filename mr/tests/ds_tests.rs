b57a583aa837bcb589fd81100d136ba3b6f88d6b
“jmin-pingu” <jonathanheromin@gmail.com>
2024-09-05 11:49:39 -0400

    Creating unittests for ds directory

--- mr/tests/ds_tests.rs
+++ mr/tests/ds_tests.rs
@@ -1,8 +1,12 @@
-use mr::ds::intermediate::Intermediate;
-
+use mr::ds::*;
+use std::{
+    time,
+    thread
+};
+use mr::ds::task::*;
 #[test]
 fn test_intermediate() {
-    let mut intermediate = Intermediate::new();
+    let mut intermediate = intermediate::Intermediate::new();
     assert_eq!(intermediate.get("foo".to_string()), None);
     intermediate.insert("foo".to_string(), "bar0".to_string());
     assert_eq!(intermediate.get("foo".to_string()).unwrap()[0], "bar0".to_string());
@@ -15,11 +19,34 @@ fn test_intermediate() {
     assert_eq!(intermediate.get("foo".to_string()).unwrap().len(), 4);
 }
 
-#[test]
-fn test1() { }
 
 #[test]
-fn test2() { }
+fn test_taskman_single_threaded() { 
+    let mut taskman = taskman::TaskManager::new();
+    let task1 = task::Task::new(vec!(String::from("a")), State::Idle, TaskType::Map);
+    let task2 = task::Task::new(vec!(String::from("b"), String::from("c")), State::Idle, TaskType::Map);
+    let task3 = task::Task::new(vec!(String::from("d"), String::from("f"), String::from("g")), State::Idle, TaskType::Map);
+    let task4 = task::Task::new(vec!(String::from("h")), State::Idle, TaskType::Map);
+    let task5 = task::Task::new(vec!(String::from("i")), State::Idle, TaskType::Map);
+    let task6 = task::Task::new(vec!(String::from("j")), State::Idle, TaskType::Map);
+
+    taskman.add_task(task1);
+    taskman.update_state(String::from("a"), State::InProgress);
+    let delay = time::Duration::from_millis(100);
+    thread::sleep(delay);
+    taskman.check_progress(String::from("a"), time::Duration::from_millis(500));
+    assert_eq!(taskman.get_task("a".to_string()).unwrap().get_state(), State::InProgress);
+    taskman.check_progress(String::from("a"), time::Duration::from_millis(100));
+    assert_eq!(taskman.get_task("a".to_string()).unwrap().get_state(), State::Idle);
+
+
+
+}
+
+#[test]
+fn test_taskman_multi_threaded() { 
+
+}
 
 #[test]
 fn test3() { }
