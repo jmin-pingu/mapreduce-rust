6074f1fc9aa45ed47ca17b71ff49c8aae387fc93
“jmin-pingu” <jonathanheromin@gmail.com>
2024-09-06 19:21:31 -0400

    Updated tests to comply with new ds interface

--- mr/tests/ds_tests.rs
+++ mr/tests/ds_tests.rs
@@ -28,7 +28,7 @@ fn test_taskman_single_threaded() {
     let mut taskman = taskman::TaskManager::new();
     let task1 = Task::new(vec!(String::from("a")), TaskType::Map, TaskID::MapID(0));
     let task2 = Task::new(vec!(String::from("b"), String::from("c")), TaskType::Map, TaskID::MapID(1));
-    let task3 = Task::new(vec!(String::from("d"), String::from("f"), String::from("g")), TaskType::Map, TaskID::MapID(2));
+    let task3 = Task::new(vec!(String::from("d"), String::from("f"), String::from("g")), TaskType::Map, TaskID::MapID(100));
     let task4 = Task::new(vec!(String::from("h")), TaskType::Map, TaskID::MapID(3));
     let task5 = Task::new(vec!(String::from("i")), TaskType::Reduce, TaskID::ReduceID);
     let task6 = Task::new(vec!(String::from("j")), TaskType::Reduce, TaskID::ReduceID);
@@ -42,21 +42,11 @@ fn test_taskman_single_threaded() {
     taskman.check_progress( time::Duration::from_millis(100));
     assert_eq!(taskman.get_task("a".to_string()).unwrap().get_state(), State::Idle);
 
-    // Size checks
-    assert_eq!(taskman.get_size(None), 1);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 1);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 0);
-
     taskman.add_task(task2);
     taskman.add_task(task3);
     taskman.add_task(task4);
     taskman.add_task(task5);
     taskman.add_task(task6);
-
-    // Size checks
-    assert_eq!(taskman.get_size(None), 6);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 4);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 2);
 
     // Get the idle tasks
     assert_eq!(taskman.get_idle_task(0, None), Some((vec!(String::from("a")), TaskType::Map)));
@@ -67,49 +57,32 @@ fn test_taskman_single_threaded() {
     assert_eq!(taskman.get_task(String::from("b")).unwrap().get_state(), State::InProgress);
     
     taskman.task_completed(String::from("a"), ReduceType::Traditional, 2, 2, 0).unwrap();
-    assert_eq!(taskman.get_size(None), 5);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 3);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 2);
 
 
     assert_eq!(taskman.get_idle_task(2, Some(TaskType::Reduce)), Some((vec!(String::from("i")), TaskType::Reduce)));
     assert_eq!(taskman.get_idle_task(3, Some(TaskType::Reduce)), Some((vec!(String::from("j")), TaskType::Reduce)));
 
     taskman.task_completed(String::from("b"), ReduceType::Traditional, 2, 2, 0).unwrap();
-    assert_eq!(taskman.get_size(None), 4);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 2);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 2);
-
-    // Complete ReduceType::Expedited, so we should have mr-100-0 and mr-100-1 of TaskType::Reduce
-    // added to taskman
-    taskman.task_completed(String::from("d"), ReduceType::Expedited, 2, 2, 100).unwrap();
-    assert_eq!(taskman.get_size(None), 5);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 1);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 4);
-
-    assert_eq!(taskman.get_task(String::from("mr-100-0")).unwrap().get_worker_id().unwrap(), 100);
-    assert_eq!(taskman.get_task(String::from("mr-100-1")).unwrap().get_worker_id().unwrap(), 100);
 
     // Get rid of the pre-existing reduce tasks
     taskman.task_completed(String::from("i"), ReduceType::Traditional, 2, 2, 0).unwrap();
     taskman.task_completed(String::from("j"), ReduceType::Traditional, 2, 2, 0).unwrap();
-    assert_eq!(taskman.get_size(None), 3);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 1);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 2);
+
+    // Complete ReduceType::Expedited, so we should have mr-100-0 and mr-100-1 of TaskType::Reduce
+    // added to taskman
+    taskman.task_completed(String::from("d"), ReduceType::Expedited, 2, 2, 0).unwrap();
+
+    // assert_eq!(taskman.get_task(String::from("mr-100-0")).unwrap().get_worker_id().unwrap(), 100);
+    // assert_eq!(taskman.get_task(String::from("mr-100-1")).unwrap().get_worker_id().unwrap(), 100);
+
 
     // Get rid of the added reduce tasks when we completed ReduceType::Expedited
     taskman.task_completed(String::from("mr-100-0"), ReduceType::Traditional, 2, 2, 0).unwrap();
     taskman.task_completed(String::from("mr-100-1"), ReduceType::Traditional, 2, 2, 0).unwrap();
-    assert_eq!(taskman.get_size(None), 1);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 1);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 0);
 
     // Finally, double-check traditional. There should be nreduce new reduce tasks since there are
     // no map tasks remaining
     taskman.task_completed(String::from("h"), ReduceType::Traditional, 2, 5, 0).unwrap();
-    assert_eq!(taskman.get_size(None), 2);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 0);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 2);
 
     // Double check the length of the lists. The different permutations of "mr-X-0" where X is a
     // map task id is just to also check the functionality of get_task
@@ -122,17 +95,8 @@ fn test_taskman_single_threaded() {
     // Complete the remaining reduce tasks
     taskman.task_completed(String::from("mr-0-0"), ReduceType::Traditional, 2, 5, 0).unwrap();
     taskman.task_completed(String::from("mr-0-1"), ReduceType::Traditional, 2, 5, 0).unwrap();
-    assert_eq!(taskman.get_size(None), 0);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 0);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 0);
-    // METHODS
-    // fn add_task(&mut self, task: Task) 
-    // fn get_task(&mut self, path: String) -> Option<Task> 
-    // fn get_idle_task(&mut self, id: i8, task_type: Option<TaskType>) -> Option<(Vec<String>, TaskType)> 
-    // fn task_completed(&mut self, task: String, reduce_type: ReduceType, nreduce: usize, nmap: usize, id: Option<i8>) -> Result<(), TaskManagerError> 
-    // fn clean(&mut self) 
-    // fn get_size(&self, task_type: Option<TaskType>) -> usize 
-    // fn check_progress(&mut self, duration: Duration) 
+
+    assert_eq!(taskman.status(), MapReduceStatus::Completed);
 }
 
 #[test]
@@ -140,9 +104,6 @@ fn test_taskman_multi_threaded() {
     let mut taskman = taskman::TaskManager::new();
     // Add several tasks
     (0..100).for_each(|i| taskman.add_task(Task::new(vec!(i.to_string()), TaskType::Map, TaskID::MapID(i))));
-    assert_eq!(taskman.get_size(None), 100);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 100);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 0);
 
     let ts_taskman = Arc::new(Mutex::new(taskman));
     let mut handles = vec![];
@@ -172,9 +133,6 @@ fn test_taskman_multi_threaded() {
     let task_ref= Arc::clone(&ts_taskman);
     {
         let safe_taskman = task_ref.lock().unwrap();
-        assert_eq!(safe_taskman.get_size(None), 100 - n as usize);
-        assert_eq!(safe_taskman.get_size(Some(TaskType::Map)), 100 - n as usize);
-        assert_eq!(safe_taskman.get_size(Some(TaskType::Reduce)), 0);
     }
 
     // Get rid of the rest of tasks
@@ -201,9 +159,6 @@ fn test_taskman_multi_threaded() {
     {
         let task_ref= Arc::clone(&ts_taskman);
         let safe_taskman = task_ref.lock().unwrap();
-        assert_eq!(safe_taskman.get_size(None), 10 as usize);
-        assert_eq!(safe_taskman.get_size(Some(TaskType::Map)), 0);
-        assert_eq!(safe_taskman.get_size(Some(TaskType::Reduce)), 10);
     }
 
 
@@ -232,9 +187,6 @@ async fn async_taskman_multithread_completion() {
     let mut taskman = taskman::TaskManager::new();
     // Add several tasks
     (0..100).for_each(|i| taskman.add_task(Task::new(vec!(i.to_string()), TaskType::Map, TaskID::MapID(i))));
-    assert_eq!(taskman.get_size(None), 100);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 100);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 0);
 
     let ts_taskman = Arc::new(Mutex::new(taskman));
     let mut completed: bool = false;
@@ -255,7 +207,7 @@ async fn async_taskman_multithread_completion() {
         {
             let task_ref= Arc::clone(&ts_taskman);
             let safe_taskman = task_ref.lock().unwrap();
-            if safe_taskman.get_size(None) == 0 {
+            if safe_taskman.status() == MapReduceStatus::Completed {
                 completed = true;
             }
         }
@@ -268,9 +220,6 @@ async fn async_taskman_multithread_delays_completion() {
     let mut taskman = taskman::TaskManager::new();
     // Add several tasks
     (0..100).for_each(|i| taskman.add_task(Task::new(vec!(i.to_string()), TaskType::Map, TaskID::MapID(i))));
-    assert_eq!(taskman.get_size(None), 100);
-    assert_eq!(taskman.get_size(Some(TaskType::Map)), 100);
-    assert_eq!(taskman.get_size(Some(TaskType::Reduce)), 0);
 
     let ts_taskman = Arc::new(Mutex::new(taskman));
     let mut completed: bool = false;
@@ -291,7 +240,7 @@ async fn async_taskman_multithread_delays_completion() {
         {
             let task_ref= Arc::clone(&ts_taskman);
             let safe_taskman = task_ref.lock().unwrap();
-            if safe_taskman.get_size(None) == 0 {
+            if safe_taskman.status() == MapReduceStatus::Completed {
                 completed = true;
             }
         }
